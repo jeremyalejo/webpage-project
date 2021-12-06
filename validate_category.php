d3cd6120625247d47a1785308be36f3b52f78478
jeremyalejo <jeremy.alejo0314@gmail.com>
2021-12-06 00:34:56 -0600

    connected the validation php to the category.php

--- validate_category.php
+++ validate_category.php
@@ -1,5 +1,4 @@
 <?php
-    $name  = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $description = filter_input(INPUT_POST, 'description');
 ?>
 
@@ -11,7 +10,7 @@
 </head>
 <body>
     <div id = "wrapper">
-        <?php if(strlen($name) == 0 || strlen($description) == 0) : ?>
+        <?php if(strlen($description) == 0) : ?>
             <h1>The title or content of the users post should have at least 1 character.</h1>
             <a href="home.php">Return home</a>
         <?php else : ?>
