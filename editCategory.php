1bf68080b77d7b880ea90b915fae0a9bd1e5002e
jeremyalejo <jeremy.alejo0314@gmail.com>
2021-12-06 00:25:41 -0600

    added a new file, editCategory.php and edited the category.php, which is a php for creating categories

--- editCategory.php
+++ editCategory.php
@@ -1,29 +1,53 @@
 <?php
-    ob_start();
-
     if(session_status() !== PHP_SESSION_ACTIVE) 
     { 
         session_start(); 
     } 
+    require('connect.php');
+    
+    $categoryID = filter_input(INPUT_POST, 'categoryID', FILTER_SANITIZE_NUMBER_INT);
+    
+    // DELETE post if delete button is pressed
+    if($_POST && !empty($_POST['btnDelete'])){
+    	$query     = "DELETE FROM categories WHERE categoryID = :categoryID";
+    	$statement = $db->prepare($query);
+    	$statement->bindValue(':categoryID', $categoryID, PDO::PARAM_INT);
 
-	$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $description = filter_input(INPUT_POST, 'description');
+    	$statement->execute();
 
-	// if post is created and title and content fields are not empty
-    if ($_POST && !empty($_POST['name']) && !empty($_POST['description'])) {
-        require('connect.php');
+    	header("Location: home.php");
+        exit();
+    } 
+    // UPDATE post if name, description and id are present in POST.
+    else if ($_POST && !empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['categoryID'])) {
+        $name  = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-        $query = "INSERT INTO categories (name, description) VALUES (:name, :description)";
+ 		$query     = "UPDATE categories SET name = :name, description = :description WHERE categoryID = :categoryID";
         $statement = $db->prepare($query);
+	   	$statement->bindValue(':name', $name);       
+	   	$statement->bindValue(':description', $description);
+	   	$statement->bindValue(':categoryID', $categoryID, PDO::PARAM_INT);
+	       
+    	$statement->execute();
 
-        $statement->bindValue(":name", $name);
-        $statement->bindValue(":description", $description);
-        
-        $statement->execute();
-      
         header("Location: home.php");
         exit();
-        ob_end_flush();
+    } 
+    else if (!empty($_GET['categoryID'])) { 
+        $categoryID = filter_input(INPUT_GET, 'categoryID', FILTER_SANITIZE_NUMBER_INT);
+       
+        $query = "SELECT * FROM categories WHERE categoryID = :categoryID";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':categoryID', $categoryID, PDO::PARAM_INT);
+        
+        $statement->execute();
+
+        $row = $statement->fetch();
+    } 
+    // if we are not UPDATING or SELECTING.
+    else {
+        $categoryID = false; 
     }
 ?>
 
@@ -34,7 +58,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Create a comment</title>
+        <title>Edit a comment</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
@@ -43,12 +67,6 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
-        <script src="https://cdn.tiny.cloud/1/yyciax6fhx89u9o5khmnp07uvayu26ys5m1hr4180h95gc4l/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-        <script>
-            tinymce.init({
-            selector: '#description'
-            });
-        </script>
     </head>
     <body>
         <!-- Navigation-->
@@ -63,7 +81,22 @@
                     <ul class="navbar-nav ms-auto py-4 py-lg-0">
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="home.php">Home</a></li>
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.php">About</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="#">Profile</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="profile.php">Profile</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link px-lg-3 py-3 py-lg-4" href="#" >Game Info<span class="fa fa-caret-down"></span></a>
+                            <ul class="dropdown-menu">
+                                <a class="nav-link px-lg-3 py-3 py-lg-3" href="agents.php"><span>Agents</span></a>
+                                <a class="nav-link px-lg-3 py-3 py-lg-3" href="gamemodes.php"><span>Game Modes</span></a>
+                                <a class="nav-link px-lg-3 py-3 py-lg-3" href="maps.php"><span>Maps</span></a>
+                            </ul>   
+                        </li>
+                        <?php if($_SESSION['currentUser'] != "Guest") :?>
+                            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="logout.php">Logout</a></li>
+                            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4">Welcome, <?= $_SESSION['currentUser'] ?></a></li>
+                        <?php else :?>
+                            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="logout.php">Login</a></li>
+                            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4">Welcome, <?= $_SESSION['currentUser'] ?></a></li>
+                        <?php endif ?>
                     </ul>
                 </div>
             </div>
@@ -74,8 +107,8 @@
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-10 col-xl-7">
                         <div class="site-heading">
-                            <h1>Create a category</h1>
-                            <span class="subheading">Make sure its useful.</span>
+                            <h1>Edit categories</h1>
+                            <span class="subheading">Fix it nicely.</span>
                         </div>
                     </div>
                 </div>
@@ -87,27 +120,31 @@
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <!-- Post preview-->
                     <div class = "form">
-                        <form action="category.php" method="post">
+                        <form action="editCategory.php" method="post">
                             <fieldset>
-                                <legend><h3>Think of a category</h3></legend>
+                                <legend><h3>Edit a category</h3></legend>
                                 <p>
                                     <label for="name">Category name</label><br>
-                                    <input type = "text" name="name" id="name" size="50" />
+                                    <select name="name" id="name" class="form-control">
+                                        <option value="<?= $row['categoryID'] ?>"><?= $row['name'] ?></option>
+                                    </select>
                                 </p>
                                 <p>
                                     <label for="description">Description</label><br>
-                                    <textarea name="description" id="description" rows="4" cols="70"></textarea>
+                                    <textarea name="description" id="description" rows="4" cols="70"><?= $row['description'] ?></textarea>
                                 </p>
                                 <p>
-                                    <input type="submit" name="submit" value="Create" />
+                                    <input type="submit" name="submit" value="Update" />
+                                    <input type="submit" name="btnDelete" value="Delete" onclick="return confirm('Are you sure you wish to delete this post?')" />
                                 </p>
                             </fieldset>
                         </form>
-                    <?php if(isset($_POST['submit'])) : ?>
+		            </div>
+
+                    <?php if(isset($_POST['btnUpdate'])) : ?>
                         <?= header("Location: validate_category.php") ?>
                         <?= exit() ?>
                     <?php endif ?>
-		            </div>
                 </div>
             </div>
         </div>
