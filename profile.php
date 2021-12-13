4483766ca748bd14317324da9842f507beed432c
jeremyalejo <jeremy.alejo0314@gmail.com>
2021-12-13 00:56:14 -0600

    ...

--- profile.php
+++ profile.php
@@ -64,7 +64,6 @@
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
-                            <
                             <span class="subheading"><?=$_SESSION['currentUser']?>'s Profile</span>
                         </div>
                     </div>
